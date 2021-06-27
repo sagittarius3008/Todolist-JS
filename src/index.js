916a94c7518fb017c2703c9959039960ef33004e
sagittarius3008 <83518855+sagittarius3008@users.noreply.github.com>
2021-06-28 06:44:13 +0900

    finish

--- src/index.js
+++ src/index.js
@@ -15,13 +15,36 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(divタグ=todo本文と完了、削除タグ)を完了リストから削除
+    const completeTarget = completeButton.parentNode; //parentNodeでdivタグと中身を選択取得
+    document.getElementById("complete-list").createElement(completeTarget);
+  });
+
+  //button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(divタグ=todo本文と完了、削除タグ)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
   // divタグの子要素に各要素を入れる
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
 
+// 未完了リストから引数の要素を削除、関数化
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target); //removeChildで削除
+};
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
